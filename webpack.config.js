c915016a4dc14fc68e859db2d82d94988c50ae8c
Nico Castañeda <nickcar13@gmail.com>
2021-04-15 12:55:08 -0500

    adding babel and babel configuration

--- webpack.config.js
+++ webpack.config.js
@@ -16,4 +16,21 @@ module.exports = {
   resolve: {
     extensions: ['.js'],
   },
+  //modulo para configurar otros paquetes en este caso babel
+  module: {
+    //reglas para un objeto
+    rules: [
+      //objeto para babel
+      {
+        //nos permite saber que extenciones vamos a usar (se trabaja con expresiones regulares)
+        test: /\.m?js$/, //utiliza cualquier extencion .mjs o .js
+        //excluir algunos archivos
+        exclude: /node_modules/,
+        //usar el loader
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
 };
