56c5809ed046527ea486bc9f59ee2465c1bee0c8
Nico Castañeda <nickcar13@gmail.com>
2021-04-24 15:39:49 -0500

    adding watch mode for webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -19,6 +19,8 @@ module.exports = {
   },
   //indicando que modo es
   mode: 'development',
+  //iniciando el modo watch
+  watch: true,
   //extensiones a trabajar
   resolve: {
     extensions: ['.js'],
